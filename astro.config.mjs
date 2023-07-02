ac90fa147489058e256d4206b2957d48d264d545
johnnylyimo <johnnylyimo@gmail.com>
2023-07-03 01:59:10 +0300

    Updated code July3

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,7 @@ import react from "@astrojs/react";
 // https://astro.build/config
 export default defineConfig({
   integrations: [react()],
-  site: 'https://molystechie.github.io',
+  site: 'https://molystechie.github.io'
 });
 
 /** Hosting on Github > https://docs.astro.build/en/guides/deploy/github/
